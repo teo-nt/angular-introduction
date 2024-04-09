369adec0c342a066edbc569f7e56710ea253d0a5
Teo <thod_ntonas@hotmail.com>
2024-04-09 04:10:32 +0300

    template driven forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ComponentInputExampleComponent } from './components/component-input-exa
 import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
 import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
+import { TemplateDrivenFormExampleComponent } from './components/template-driven-form-example/template-driven-form-example.component';
 
 export const routes: Routes = [
     { path: 'event-bind-example', component: EventBindExampleComponent }, 
@@ -14,5 +15,6 @@ export const routes: Routes = [
     {path: 'component-input-example', component: ComponentInputExampleComponent},
     {path: 'for-directive-example', component: ForDirectiveExampleComponent},
     {path: 'simple-datatable-example', component: SimpleDatatableExampleComponent},
-    {path: 'component-output-example', component: ComponentOutputExampleComponent}
+    {path: 'component-output-example', component: ComponentOutputExampleComponent},
+    {path: 'template-driven-form-example', component: TemplateDrivenFormExampleComponent}
 ];
--- src/app/components/list-group-menu/list-group-menu.component.ts
+++ src/app/components/list-group-menu/list-group-menu.component.ts
@@ -15,6 +15,7 @@ export class ListGroupMenuComponent {
     {text: '@for Directive Example', routerLink: 'for-directive-example'},
     {text: 'Event Bind Example', routerLink: 'event-bind-example'},
     {text: 'Simple Datatable Example', routerLink: 'simple-datatable-example'},
-    {text: 'Component Output Example', routerLink: 'component-output-example'}
+    {text: 'Component Output Example', routerLink: 'component-output-example'},
+    {text: 'Template Driven Form Example', routerLink: 'template-driven-form-example'}
   ]
 }
